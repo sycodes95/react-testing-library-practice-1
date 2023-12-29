c95aa62facce416477a0dc698b699cf046be3cf4
sycodes95 <sycodes95@gmail.com>
2023-12-29 14:23:09 -0800

    made test work

--- src/userForm.test.tsx
+++ src/userForm.test.tsx
@@ -14,8 +14,43 @@ test('it shows two inputs and a button', () => {
 
 })
 
-test('it calls on setUserList when form is submitted', () => {
-  //NOT THE BEST IMPLEMENTATION
+interface User {
+  name: string;
+  email: string;
+}
 
+test('it calls on onUserAdd when form is submitted', async () => {
+  //NOT THE BEST IMPLEMENTATION
+  // Try to render my component
+
+  const argList = [];
+
+  const callback = (userData) => {
+    argList.push(userData);
+  };
   
+  
+  render(<UserForm onUserAdd={callback} />)
+
+  const [nameInput , emailInput] = screen.getAllByRole('textbox')
+
+  // Find the two inputs 
+
+  // SImulate typing in a name 
+  await user.click(nameInput);
+  await user.keyboard('jane')  
+
+  // Simulate typing in an email
+  await user.click(emailInput)
+  await user.keyboard('jane@jane.com')
+  // Find the button 
+
+  const button = screen.getByRole('button')
+  // Simulate clicking the button
+  await user.click(button)
+  // Assertion to make sure 'onUserAdd' is called.
+  expect(argList).toHaveLength(1);
+  expect(argList[0]).toEqual({ name: 'jane', email: 'jane@jane.com'})
+
+
 })
--- src/userForm.tsx
+++ src/userForm.tsx
@@ -24,7 +24,7 @@ export default function UserForm ({ onUserAdd } : UserFormProps) {
     setUserForm((prev) => ({ ...prev, [type]: value}));
   };
   return (
-    <form className='flex flex-col items-center justify-center gap-4 p-4 border border-black rounded-lg' onSubmit={(e)=> handleUserFormSubmit(e)}>
+    <form className='flex flex-col items-center justify-center gap-4 p-4 border border-black rounded-lg' onSubmit={handleUserFormSubmit}>
       <input className='p-2 rounded-lg' type="text" name="name" placeholder='name' onChange={(e) => handleUserFormChange(e.target.value, e.target.name)} value={userForm.name}/>
       <input className='p-2 rounded-lg' type="text" name="email" placeholder='email' onChange={(e) => handleUserFormChange(e.target.value, e.target.name)} value={userForm.email}/>
       <button className='p-4 text-white bg-black rounded-lg'>Submit</button>
